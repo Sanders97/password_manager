4afb368fc2db1b4adc407b499038ae5baa472f43
unknown <mrwsanders1@gmail.com>
2018-10-20 00:08:58 -0400

    beggining framework for random string generation

--- files/parameters.py
+++ files/parameters.py
@@ -1,3 +1,4 @@
 math.ceil(25)
 # Im not quite sure what to add here to make  the code tie together yet, but I will work on it more later...
 math.floor(5)
+choices(population, weights=None, *, cum_weights=None, k=5)
