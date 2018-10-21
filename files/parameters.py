ff1d3d1effbf5a93529d36c21ee98c80377a0039
unknown <mrwsanders1@gmail.com>
2018-10-20 23:54:25 -0400

    added some notes to try and get working later

--- files/parameters.py
+++ files/parameters.py
@@ -2,3 +2,13 @@ math.ceil(25)
 # Im not quite sure what to add here to make  the code tie together yet, but I will work on it more later...
 math.floor(5)
 choices(population, weights=None, *, cum_weights=None, k=5)
+# >>> import string
+# >>> import random
+# >>> def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+# ...    return ''.join(random.choice(chars) for _ in range(size))
+# ...
+# >>> id_generator()
+# 'G5G74W'
+# >>> id_generator(3, "6793YUIO")
+# 'Y3U' 
+# Notes from stackoverflow, not sure how to get this working yet, but wanted to save it here.
