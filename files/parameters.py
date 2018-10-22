14c09a38509cf6b5d188c33093888174eca97b5f
unknown <mrwsanders1@gmail.com>
2018-10-21 23:51:39 -0400

    more notes added and attempted to add some code to tie the whole thing together

--- files/parameters.py
+++ files/parameters.py
@@ -2,6 +2,9 @@ math.ceil(25)
 # Im not quite sure what to add here to make  the code tie together yet, but I will work on it more later...
 math.floor(5)
 choices(population, weights=None, *, cum_weights=None, k=5)
+def string()
+# I'm guessing lines 7-16 are a callback in which I would be using code I previosly wrote in order to get my block of code to run.
+# however, since I don't have any code and this is my first page, that would mean I needed to write the code myself in order for it to be called back later. that is what I'm attempting on line 5
 # >>> import string
 # >>> import random
 # >>> def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
