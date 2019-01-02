692bd978f1d71a66dd17851581bcd477039b5ea5
unknown <mrwsanders1@gmail.com>
2019-01-02 01:53:14 -0500

    added psuedocode/notes for python files

--- files/parameters.py
+++ files/parameters.py
@@ -6,3 +6,5 @@ max_char = 12
 allchar = string.ascii_letters + string.punctuation + string.digits
 password = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
 print "This is your password : ",password
+# upload code to db
+# check to make sure password hasnt been used in 6 months
