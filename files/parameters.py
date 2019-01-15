ae4be159abc1e219cd824c16d1c33da13837e655
unknown <mrwsanders1@gmail.com>
2019-01-15 02:23:04 -0500

    added some lines to python code

--- files/parameters.py
+++ files/parameters.py
@@ -8,3 +8,8 @@ password = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
 print "This is your password : ",password
 # upload code to db
 # check to make sure password hasnt been used in 6 months
+def __new__(dbUpload)
+if password == dbPassword:
+	print "already uploaded"
+else:
+	pass
