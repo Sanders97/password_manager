f5b796bc071acad48dd203a2578b0dcff2d228be
unknown <mrwsanders1@gmail.com>
2019-01-17 02:22:08 -0500

    tweaked python files... 'i think' and fixed footer

--- files/parameters.py
+++ files/parameters.py
@@ -8,10 +8,10 @@ password = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
 print "This is your password : ",password
 # upload code to db
 # check to make sure password hasnt been used in 6 months
-def __new__(dbUpload)
-if password == dbPassword:
-	print "already uploaded"
-else: print "new password added"
-upload password tp db 
+	def __new__(dbUpload):
+	if password == dbPassword:
+		print "already uploaded"
+	else: print "new password added"
+	upload password tp db 
 	
 
