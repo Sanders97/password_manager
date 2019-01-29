83a2239d2a516ef31cdafdaa94b5725321197c49
unknown <mrwsanders1@gmail.com>
2019-01-29 03:51:49 -0500

    began laying down js for buttons and functionality

---
+++ files/Front end/js/mainpage.js
@@ -0,0 +1,12 @@
+if (buttonA) {
+	selection = true
+}create password 
+
+if (buttonB) {
+	selection = true
+}save password 
+
+if (buttonA) {
+	selection = true
+}open saved password list password 
+
